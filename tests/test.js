e9b1217cdc819e8a9204440c043855f5e2c18a3d
lamnt95 <34705561+lamnt95@users.noreply.github.com>
2022-09-14 15:29:47 +0700

    a

--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,5 @@
-import assert from "assert";
-import anchor from "@project-serum/anchor";
+const assert = require("assert")
+const anchor = require("@project-serum/anchor")
 
 const { SystemProgram } = anchor.web3
 
