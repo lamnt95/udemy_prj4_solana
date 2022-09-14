a742b55061e1923e414c2da36f9215b3e1055e11
lamnt95 <34705561+lamnt95@users.noreply.github.com>
2022-09-14 15:33:52 +0700

    A

--- tests/test.ts
+++ tests/test.ts
@@ -4,22 +4,21 @@ const { SystemProgram } = anchor.web3;
 
 describe("test", () => {
   const provider = anchor.Provider.local();
-  anchor.setProvider(provider);
-  const calculator = anchor.web3.Keypair.generate();
-  const program = anchor.workspace.UdemyPrj4Solana;
-  if ("Creates a calculator", async () => {
-    await program.rpc.create(
-      "Hello solana",
-      {
-        accounts: {
-          calculator: calculator.publicKey,
-          user: provider.wallet.publicKey,
-          systemProgram: SystemProgram.programId
-        },
-        signer: [calculator]
-      }
-    );
-    const account = await program.account.calculator.fetch(calculator.publicKey);
-    assert.ok(account.greeting === "Hello solana")
-  });
+	anchor.setProvider(provider);
+	const calculator = anchor.web3.Keypair.generate();
+	const program = anchor.workspace.UdemyPrj4Solana;
+	it("Creates a calculator", async () => {
+		await program.rpc.create("Welcome to Solana", {
+			accounts: {
+				calculator: calculator.publicKey,
+				user: provider.wallet.publicKey,
+				systemProgram: SystemProgram.programId,
+			},
+			signers: [calculator],
+		});
+		const account = await program.account.calculator.fetch(
+			calculator.publicKey
+		);
+		assert.ok(account.greeting === "Welcome to Solana");
+	});
 })
